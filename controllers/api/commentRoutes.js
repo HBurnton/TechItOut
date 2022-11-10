027240f43ce4bed705419e3178e38def47f00999
hburnton <hburnton@gmail.com>
2022-11-09 17:02:30 -0700

    add comment beginning routes

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,4 +1,30 @@
 const router = require('express').Router();
 const { Comment } = require('../../models');
 
+router.get('/', async (res, res) => {
+  try {
+    const comments = await Comment.findAll({});
+    res.status(200).json(comments);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const comment = await Comment.findOne({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (comment) {
+      res.status(200).json(comment);
+    } else {
+      res.status(404).json('Comment Not Found');
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
